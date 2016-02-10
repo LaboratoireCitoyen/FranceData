dbe9d0f93b78c69d1fa2b2eeffcb470ad348731d
Nicolas Joyard <joyard.nicolas@gmail.com>
2016-02-10 22:15:58 +0100

    Enhance depute spider
    - Add photo URL on nosdeputes.fr
    - Extract phone numbers from addresses
    - Geocode first non-assembly address using geocoder on adresse.data.gouv.fr
      (only the first, highest-ranked result is kept)

--- francedata/spiders/depute.py
+++ francedata/spiders/depute.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import json
 import re
+from urllib import quote
 import urlparse
 
 from scrapy import Request
@@ -9,9 +10,12 @@ from scrapy.contrib.spiders import CrawlSpider
 
 class DeputeSpider(CrawlSpider):
     name = "deputespider"
+    geocode_url = 'http://api-adresse.data.gouv.fr/search/?q=%s'
+    photo_url = 'http://www.nosdeputes.fr/depute/photo/%s'
 
     allowed_domains = [
-        "www.nosdeputes.fr"
+        "www.nosdeputes.fr",
+        "api-adresse.data.gouv.fr"
     ]
 
     start_urls = (
@@ -32,8 +36,47 @@ class DeputeSpider(CrawlSpider):
                           callback=self.parse_depute)
 
     def parse_depute(self, response):
-        depute = json.loads(response.body_as_unicode());
+        depute = json.loads(response.body_as_unicode())
         if 'depute' in depute:
             depute = depute['depute']
+
+        depute['photo_url'] = self.photo_url % depute['slug']
+
+        req = None
+
+        for ad in depute['adresses']:
+            adresse = ad['adresse']
+
+            pattern = ur'Télé(phone|copie)\s*:\s*(\d[0-9 ]+\d)'
+            for telm in re.finditer(pattern, adresse):
+                if telm.group(1) == 'phone':
+                    ad['tel'] = telm.group(2)
+                else:
+                    ad['fax'] = telm.group(2)
+
+            lad = adresse.lower()
+            if not req and not lad.startswith(u'assemblée nationale'):
+                trimmed = re.sub(pattern, '', adresse)
+                req = Request(url=self.get_geocode_url(adresse),
+                              callback=self.parse_geocode)
+
+                req.meta['depute'] = depute
+                req.meta['adresse'] = ad
+
+        if req is not None:
+            yield req
+        else:
+            yield depute
+
+    def get_geocode_url(self, q):
+        return self.geocode_url % quote(q.encode('utf-8'))
+
+    def parse_geocode(self, response):
+        depute = response.meta['depute']
+        adresse = response.meta['adresse']
+
+        geo = json.loads(response.body_as_unicode())
+        if 'features' in geo and len(geo['features']) > 0:
+            adresse['geo'] = geo['features'][0]
 
         yield depute
